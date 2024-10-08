bd31a21196d0f07ada3ac3f4542b0427ff9d22bf
ry129 <0111cs211146.riya@gmail.com>
2024-10-08 15:42:51 +0530

    Assessment Submission

--- program1.py
+++ program1.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def isValid(self, s):
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in bracket_map.values():  
+                stack.append(char)
+            elif char in bracket_map.keys():  
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
+    
+        return len(stack) == 0 
         """
         :type s: str
         :rtype: bool
